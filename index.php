badbe386cf7a1c4fb595f9029173ddcc29e4315c
Tiago dos Santos <otiagosantos.code@gmail.com>
2025-02-17 19:38:49 -0300

    add historia section with partial styles.

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="/styles/header.css" />
     <link rel="stylesheet" href="/styles/hero.css" />
     <link rel="stylesheet" href="/styles/destinos.css" />
+    <link rel="stylesheet" href="/styles/historia.css" />
 
     <script src="/scripts/global.js" defer></script>
     <script src="/scripts/header.js" defer></script>
@@ -103,6 +104,58 @@
             <div id="destinos_content" class="content-width">
                 <div></div>
                 <!-- JS INJECTION -->
+            </div>
+        </section>
+        <section id="historia">
+            <div id="historia_title">
+                <h3 class="title">História da Rotas Travel</h3>
+            </div>
+            <div id="historia_content" class="content-width">
+                <div id="historia_left-side-content">
+                    <figure class="card">
+                        <img src="/assets/images/foto-veronica-pg 3.png" alt="">
+
+                        <figcaption>Verônica Matos | CEO</figcaption>
+                    </figure>
+                </div>
+                <div id="historia_right-side-content">
+
+                    <div class="historia_quote">
+                        <blockquote>
+                            <p><cite>VIAJAR É TROCAR A ROUPA DA ALMA<cite></p>
+                        </blockquote>
+                        <p>—Mario Quintana</p>
+                    </div>
+
+                    <p>Sempre que eu viajava, eu queria capturar rudo na lente da minha câmera, mas a cada clique percebia e sentia vibrar meu desejo que mais pessoas tinha que ver o mesmo que eu estava vendo e sentindo, porque não conseguia guardar exatamente como era, que só tirar fotos e fazer vídeos não seria o suficiente para as pessoas realmente viverem aquele momento naquele lugar que eu também estava vivenciando.</p>
+
+                    <p>A Rotas Travel nasceu do desejo de oferecer experiências únicas e memoráveis, conectando pessoas aos encantos e riquezas naturais, principalmente na América do Sul que é a minha paixão.</p>
+
+                    <p>Nos tornamos uma agência de turismo com foco em explorar cada detalhe que esse continente fascinante tem para nos oferecer.</p>
+
+                    <p>Nosso compromisso juntamente com nossas experiências nos destinos é garantir que cada viagem seja mais que apenas uma visita e destinos turísticos, mas sim, uma imersão autêntica e segura. Cuidamos de tudo, desde o planejamento até o suporte durante toda a jornada, para que nossos viajantes possam desfrutar plenamente da experiência com todo o compromisso que podemos oeferecer e tranquilidade para os nossos biajantes durante todo o percurso da viagem.</p>
+
+                    <span class="bold uppercase">Experiências:</span>
+
+                    <p>Somos especialistas em destinos icônicos na América do Sul, oferecendo viagens contemplativas e cheias de aventura.</p>
+
+                    <!-- <ul id="historia_experiencias-list">
+                        <li>Patatgônia Argentina e Chilena</li>
+
+                        <li>Vinhedos de Santiago, Chile</li>
+
+                        <li>Deserto do Atacama e Kalama</li>
+
+                        <li>Salar de Uyuni, Bolívia</li>
+                    </ul> -->
+
+                    <!-- <p>Com roteiros que vão do ecoturismo e aventura ao turismo cultural e gastronômico, <br> garantimos experiências completas para todos os estilos de viagem.</p>
+
+                    <span>Lua de mel | Casal | Melhor Idade | Amigos | Aventura | Cruzeiro | Família</span> -->
+                </div>
+            </div>
+            <div id="historia_bg">
+                <img src="/assets/images/logo-com-pouca-opacidade-pg 3.png" alt="">
             </div>
         </section>
 
