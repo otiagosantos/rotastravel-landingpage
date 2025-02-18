389eb4328f06d4ac57f78f7e2efed200e9a4a12b
Tiago dos Santos <otiagosantos.code@gmail.com>
2025-02-18 15:41:53 -0300

    start gallery app test.

---
+++ destinos-gallery-test.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="/styles/global.css" />
+    <link rel="stylesheet" href="/styles/destinos-gallery.css" />
+
+    <script src="/scripts/destinos-gallery.js" defer></script>
+</head>
+
+<body>
+    <h1>Gallery</h1>
+
+    <div id="destinos_gallery-app">
+        <div id="gallery-app_card-list">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+        <div id="gallery-app_navmenu">
+            <button class="btn"><</button>
+            <button class="btn">Ver Mais</button>
+            <button class="btn">></button>
+        </div>
+    </div>
+</body>
+
+</html>
